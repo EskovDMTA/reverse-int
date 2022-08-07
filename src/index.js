c05bd2faa62bd5ead56c76e3084f125f56f856b5
LastBuilder <eskovdmt@gmail.com>
2022-08-07 17:54:00 +0300

    fix: fix index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    if (n<0){
+        let nString = String(n).slice(1);
+        let nReverse = nString.split('').reverse().join('');
+        let nNumber = Number(nReverse);
+        return nNumber
+    } else {
+    let nString = String(n);
+    let nReverse = nString.split('').reverse().join('');
+    let nNumber = Number(nReverse)
+    return nNumber
+    }
 }
